a3d861b8879959ec30df9d6c52cf2f078b79ef18
Sharkbyteprojects <40953479+Sharkbyteprojects@users.noreply.github.com>
2019-03-18 20:50:49 +0100

    Create sendmail_test.py

---
+++ sendmail_test.py
@@ -0,0 +1,20 @@
+import requests
+import time
+froms = 'python-mailer@sharkbytenetwork.net'
+print("Sharkbyteprojects Mailing Service Free")
+to = "sharkbyteprojects@protonmail.com"
+subject = "test"
+message = "TEst OK!"
+url = 'http://services.sharkbyte.bplaced.net/send.php?user=python'
+payload = {'from': froms,
+'to': to,
+'sub': subject,
+'messages': message}
+print(" ")
+print(" ")
+print("HTML CODE FROM ANSWER:")
+files = {}
+headers = {}
+response = requests.request('POST', url, headers = headers, data = payload, allow_redirects=False)
+print(response.text)
+time.sleep(10)
